c345811b1c5f3422edb5c74c3633a9bab65caac8
aknabob <aknabob@gmail.com>
2026-08-29 08:29:35 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Akeem's Timetable Generator - PWA
+
+Ready for GitHub Pages and PWA Builder.
+
+## Files
+- index.html, manifest.json, sw.js
+- icons/ (72-512px + maskable)
+
+## Steps
+1. Create GitHub repo
+2. Upload all files (keep icons folder)
+3. Settings > Pages > Deploy from branch main/root
+4. Copy Pages URL to https://www.pwabuilder.com/ > Build My PWA > Android/Windows/iOS
+
+Test offline support via Chrome DevTools > Application > Service Workers.
---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'akeem-timetable-v1.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-192x192-maskable.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './icons/icon-512x512-maskable.png',
+  './icons/icon-180x180.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (e.request.method === 'GET' && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
